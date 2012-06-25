e558c2bc77b862562c79d79a53bb24027df159ad
verticalduck <none@none>
2012-06-25 18:28:06 +0200

    plume, tool update

--- scenes/vs_plume.py
+++ scenes/vs_plume.py
@@ -1,5 +1,8 @@
+#
+# Example scene for mesh-based smoke simulations using Vortex Sheets
+#
+
 from manta import *
-import os, shutil, math
 
 res = 64
 scale = 0.2
@@ -31,7 +34,6 @@ noise.valOffset = 0.75
 noise.timeAnim = 0.2
 
 mesh = s.create(VortexSheetMesh)
-
 vp = s.create(VortexParticleSystem)
 
 flags.initDomain()
@@ -41,45 +43,48 @@ if (GUI):
     gui = Gui()
     gui.show()
 
+# inflow mesh
 source = s.create(Cylinder, center=gs*vec3(0.5,0.13,0.5), radius=res*0.14, z=gs*vec3(0, 0.03, 0))
 mesh.fromShape(source)
 subdivideMesh(mesh=mesh, minAngle=0.01, minLength=scale, maxLength=2*scale)
 
+# fix the nodes at the bottom of mesh
 fixedRegion = s.create(Box, center=gs*vec3(0.5,0.09,0.5), size=gs*vec3(0.4,0.03,0.4))
     
 #main loop
-for t in range(250):
+for t in range(180):
+    # seed smoke within the source region and apply inflow velocity condition
     densityNoiseInflow(flags=flags, density=density, noise=noise, shape=source, scale=1, sigma=0.5)
-    
     source.applyToGrid(grid=vel, value=velInflow)
     advectSemiLagrange(flags=flags, vel=vel, grid=density, order=2)
-    
     markAsFixed(mesh=mesh, shape=fixedRegion)
 
+    # advect mesh in large-scale grid
     mesh.calcCirculation()        
     mesh.advectInGrid(vel=vel, flaggrid=flags, integrationMode=IntRK4)  
     mesh.calcVorticity()
     
-    #smoothVorticity(mesh=mesh, iter=50, alpha=1.0, sigma=0.6)        
-    filterVorticityCuda(mesh=mesh, sigma=1.6)
+    # vortex sheet integration
+    #filterVorticityCuda(mesh=mesh, sigma=1.6)
+    smoothVorticity(mesh=mesh, iter=50, alpha=1.0, sigma=0.6) # fast approximation for Gauss kernel
     meshApplyBuoyancyLocalCuda(mesh=mesh, scale=0.6*1e-2, cutoffCells=5, regularization=0.5)
     mesh.calcVorticity()
     
+    # mesh cosmetics
     smoothMesh(mesh=mesh, strength=1e-4, steps=1)
     subdivideMesh(mesh=mesh, minAngle=0.01, minLength=scale, maxLength=2*scale, cutTubes=True)
     killSmallComponents(mesh=mesh, elements=20)
     
+    # base solver
     advectSemiLagrange(flags=flags, vel=vel, grid=vel, order=2)
     setWallBcs(flags=flags, vel=vel)
-    
     addBuoyancy(density=density, vel=vel, gravity=gravity*dx, flags=flags)
-    
     solvePressure(flags=flags, vel=vel, pressure=pressure)
     setWallBcs(flags=flags, vel=vel)
     
+    # vorticity source terms
     vorticitySource(mesh=mesh, gravity=gravity, scale=0.1, maxAmount=200)
     
     mesh.save('d%04d.bobj.gz' % t)
-    s.printTimings()
     s.step()
     
