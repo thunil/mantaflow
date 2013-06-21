50f7039cbecb9a0a698ada72e76d1324af840b85
thunil <thuereyn@inf.ethz.ch>
2013-06-20 21:12:33 -0700

    - added vec3 noise function
    --HG--
    branch : waveletTurb

--- source/plugin/initplugins.cpp
+++ source/plugin/initplugins.cpp
@@ -22,22 +22,45 @@ namespace Manta {
     
 //! Apply noise to grid
 KERNEL 
-void KnApplyNoise(FlagGrid& flags, Grid<Real>& dens, WaveletNoiseField& noise, Grid<Real>& sdf, Real scale, Real sigma) 
+void KnApplyNoise(FlagGrid& flags, Grid<Real>& density, WaveletNoiseField& noise, Grid<Real>& sdf, Real scale, Real sigma) 
 {
     if (!flags.isFluid(i,j,k) || sdf(i,j,k) > sigma) return;
     Real factor = clamp(1.0-0.5/sigma * (sdf(i,j,k)+sigma), 0.0, 1.0);
     
     Real target = noise.evaluate(Vec3(i,j,k)) * scale * factor;
-    if (dens(i,j,k) < target)
-        dens(i,j,k) = target;
+    if (density(i,j,k) < target)
+        density(i,j,k) = target;
 }
 
-//! Init noise-moduled density inside shape
+//! Init noise-modulated density inside shape
 PYTHON void densityInflow(FlagGrid& flags, Grid<Real>& density, WaveletNoiseField& noise, Shape* shape, Real scale=1.0, Real sigma=0)
 {
     Grid<Real> sdf = shape->computeLevelset();    
     KnApplyNoise(flags, density, noise, sdf, scale, sigma);
 }
+
+
+    
+//! Apply vector noise to grid
+KERNEL 
+void KnApplyNoiseVec(FlagGrid& flags, Grid<Vec3>& target, WaveletNoiseField& noise, Grid<Real>& sdf, Real scale, Real sigma) 
+{
+    if (!flags.isFluid(i,j,k) || sdf(i,j,k) > sigma) return;
+    Real factor = clamp(1.0-0.5/sigma * (sdf(i,j,k)+sigma), 0.0, 1.0);
+    
+    Vec3 noiseVec3 = noise.evaluateVec(Vec3(i,j,k)) * scale * factor;
+
+	target(i,j,k) += noiseVec3;
+    //if (dens(i,j,k) < target) dens(i,j,k) = target;
+}
+
+//! Apply vector-based wavelet noise to target grid
+PYTHON void applyNoiseVec3(FlagGrid& flags, Grid<Vec3>& target, WaveletNoiseField& noise, Shape* shape, Real scale=1.0, Real sigma=0)
+{
+    Grid<Real> sdf = shape->computeLevelset();    
+    KnApplyNoiseVec(flags, target, noise, sdf, scale, sigma);
+}
+
 
     
 } // namespace
