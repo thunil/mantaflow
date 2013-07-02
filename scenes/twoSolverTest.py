ac48316847608e7277920111e8a4d0ca1b4d1588
thunil <none@none>
2013-07-01 17:57:30 -0700

    clean up and move of wavelet turbulence code
    --HG--
    branch : waveletTurb

--- scenes/twoSolverTest.py
+++ scenes/twoSolverTest.py
@@ -8,10 +8,10 @@ import os, shutil, math, sys
 # dimension two/three d
 dim = 2
 # how much to upres the XL sim?
-upres = 3
+upres = 2
 
 # solver params
-res = 60
+res = 120
 gs = vec3(res,int(1.5*res),res)
 # gs = vec3(res,res,res)
 if (dim==2): gs.z = 1  # 2D
@@ -20,9 +20,9 @@ sm = Solver(name='main', gridSize = gs, dim=dim)
 
 
 # sm.timestep = 1.0
-sm.timestep = 2.0
+sm.timestep = 1.5
 
-velInflow = vec3(1, 0, 0)
+velInflow = vec3(2, 0, 0)
 #velInflow = vec3(1, 1, 1)
 
 # prepare grids
@@ -33,19 +33,20 @@ pressure = sm.create(RealGrid)
 energy   = sm.create(RealGrid)
 
 # inflow noise field
-noise = sm.create(NoiseField, fixedSeed=765)
+noise = sm.create(NoiseField, fixedSeed=265, loadFromFile=True)
 noise.posScale = vec3(20)
 noise.clamp = True
 noise.clampNeg = 0
 noise.clampPos = 2
 noise.valScale = 1
 noise.valOffset = 0.075
-noise.timeAnim = 0.2
+noise.timeAnim = 0.6
 
 flags.initDomain()
 flags.fillGrid()
 
-source = sm.create(Cylinder, center=gs*vec3(0.2,0.2,0.5), radius=res*0.1, z=gs*vec3(0.1, 0, 0))
+source = sm.create(Cylinder, center=gs*vec3(0.3,0.2,0.5), radius=res*0.081, z=gs*vec3(0.081, 0, 0))
+sourceVel = sm.create(Cylinder, center=gs*vec3(0.3,0.2,0.5), radius=res*0.15, z=gs*vec3(0.15, 0, 0))
 
 
 # larger solver, recompute sizes...
@@ -64,9 +65,9 @@ xl_weight  = xl.create(RealGrid)
 xl_flags.initDomain()
 xl_flags.fillGrid()
 
-xl_source = xl.create(Cylinder, center=xl_gs*vec3(0.2,0.2,0.5), radius=xl_gs.x*0.1, z=xl_gs*vec3(0.1, 0, 0))
+xl_source = xl.create(Cylinder, center=xl_gs*vec3(0.3,0.2,0.5), radius=xl_gs.x*0.081, z=xl_gs*vec3(0.081, 0, 0))
 
-xl_noise = xl.create(NoiseField, fixedSeed=765)
+xl_noise = xl.create(NoiseField, fixedSeed=265, loadFromFile=True)
 xl_noise.posScale = vec3(20)
 xl_noise.clamp = noise.clamp
 xl_noise.clampNeg = noise.clampNeg
@@ -76,9 +77,14 @@ xl_noise.valOffset = noise.valOffset
 xl_noise.timeAnim  = noise.timeAnim
 
 # wavelet turbulence noise
-wltnoise = sm.create(NoiseField)
-wltnoise.posScale = vec3(gs.x) # scale according to lowres sim
+wltnoise = sm.create(NoiseField, loadFromFile=True)
+# scale according to lowres sim , smaller numbers mean larger vortices
+wltnoise.posScale = vec3( int(0.5*gs.x) )
 wltnoise.timeAnim = 0.1
+
+wltnoise2 = sm.create(NoiseField, loadFromFile=True)
+wltnoise2.posScale = vec3( int(1*gs.x) )
+wltnoise2.timeAnim = 0.1
 
 
 
@@ -87,7 +93,7 @@ if (GUI):
     gui.show()
 
 #main loop
-for t in range(200):
+for t in range(150):
     
     curt = t * sm.timestep
     #sys.stdout.write( "Curr t " + str(curt) +" \n" )
@@ -96,22 +102,27 @@ for t in range(200):
     advectSemiLagrange(flags=flags, vel=vel, grid=density, order=2)    
     advectSemiLagrange(flags=flags, vel=vel, grid=vel, order=2)
     
-    if (curt>=0 and curt<30) or (curt>60 and curt<90):
+    applyInflow=False
+    if (curt>=0 and curt<75):
         densityInflow( flags=flags, density=density, noise=noise, shape=source, scale=1, sigma=0.5 )
-        source.applyToGrid( grid=vel , value=velInflow )
+        sourceVel.applyToGrid( grid=vel , value=velInflow )
+        applyInflow=True
     
     setWallBcs(flags=flags, vel=vel)    
     addBuoyancy(density=density, vel=vel, gravity=vec3(0,-1e-3,0), flags=flags)
     
     #applyNoiseVec3( flags=flags, target=vel, noise=noise, scale=1 ) # just to test, add everywhere...
     
-    solvePressure(flags=flags, vel=vel, pressure=pressure)
+    solvePressure(flags=flags, vel=vel, pressure=pressure , openBound='Y', \
+        cgMaxIterFac=0.5, cgAccuracy=0.01 )
     setWallBcs(flags=flags, vel=vel)
     
     computeEnergy(flags=flags, vel=vel, energy=energy)
     computeWaveletCoeffs(energy)
+    #computeVorticity( vel=vel, vorticity=vort, norm=energy);
+    #computeStrainRateMag( vel=vel, vorticity=vort, mag=energy);
     
-#    density.save('densitySm_%04d.vol' % t)
+    #density.save('densitySm_%04d.vol' % t)
     
     sm.step()
     
@@ -121,17 +132,20 @@ for t in range(200):
     interpolateGrid( target=xl_weight, source=energy )
     interpolateMACGrid( source=vel, target=xl_vel )
     
-    applyNoiseVec3( flags=xl_flags, target=xl_vel, noise=wltnoise, scale=2.5, weight=xl_weight)
+    applyNoiseVec3( flags=xl_flags, target=xl_vel, noise=wltnoise, scale=1. , weight=xl_weight)
+    # manually apply second octave for now
+    applyNoiseVec3( flags=xl_flags, target=xl_vel, noise=wltnoise2, scale=0.6 , weight=xl_weight)
     
     for substep in range(upres):
         advectSemiLagrange(flags=xl_flags, vel=xl_vel, grid=xl_density, order=2)    
     # advectSemiLagrange(flags=xl_flags, vel=xl_vel, grid=xl_vel, order=2)
     
-    if (curt>=0 and curt<30) or (curt>60 and curt<90):
+    if (applyInflow):
          densityInflow( flags=xl_flags, density=xl_density, noise=xl_noise, shape=xl_source, scale=1, sigma=0.5 )
          # source.applyToGrid( grid=xl_vel , value=velInflow )
     
-#    xl_density.save('densityXl_%04d.vol' % t)
+    #xl_density.save('densityXl08_%04d.vol' % t)
     
-    xl.step()
+    xl.step()    
+    gui.screenshot( 'densXl_wlt_%04d.png' % t );
 
