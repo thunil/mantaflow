f25706db152c53c0b57e19e3e3f0ce7a02098521
thunil <thuereyn@inf.ethz.ch>
2013-09-05 21:22:36 -0700

    minor update of freesurface test, supports 2d
    --HG--
    branch : flipExtensions
    extra : rebase_source : 94c8c35fff9fdbbf1e6bfaa1b6cf34f0fec21ab6

--- scenes/freesurface.py
+++ scenes/freesurface.py
@@ -5,9 +5,12 @@
 from manta import *
 
 # solver params
+dim = 3
 res = 64
 gs = vec3(res,res,res)
-s = Solver(name='main', gridSize = gs)
+if (dim==2):
+	gs.z=1
+s = Solver(name='main', gridSize = gs, dim=dim)
 s.timestep = 0.25
 ghostFluid = True
 accuracy = 5e-5
@@ -20,16 +23,18 @@ mesh = s.create(Mesh)
 
 # scene setup
 flags.initDomain(boundaryWidth=0)
-drop = s.create(Sphere, center=gs*vec3(0.5,0.5,0.5), radius=res*0.15)
 basin = s.create(Box, p0=gs*vec3(0,0,0), p1=gs*vec3(1,0.2,1))
+drop  = s.create(Sphere, center=gs*vec3(0.5,0.5,0.5), radius=res*0.15)
 phi = basin.computeLevelset()
 phi.join(drop.computeLevelset())
 flags.updateFromLevelset(phi)
-
+        
 if (GUI):
     gui = Gui()
     gui.show()
+    #gui.pause()
     
+
 #main loop
 for t in range(2000):
     
@@ -44,18 +49,21 @@ for t in range(2000):
     
     # pressure solve
     setWallBcs(flags=flags, vel=vel)
-    #setLiquidBcs(flags=flags, vel=vel)
     if ghostFluid:
         solvePressure(flags=flags, vel=vel, pressure=pressure, cgMaxIterFac=0.5, cgAccuracy=accuracy, useResNorm=False, phi=phi)
     else:
         solvePressure(flags=flags, vel=vel, pressure=pressure, cgMaxIterFac=0.5, cgAccuracy=accuracy, useResNorm=False)
-    #setLiquidBcs(flags=flags, vel=vel)
     setWallBcs(flags=flags, vel=vel)
     
     # note: these meshes are created by fast marching only, should smooth
-    #       geometry and normals before rendering
-    phi.createMesh(mesh)
-    #mesh.save('phi%04d.bobj.gz' % t)
+    #       geometry and normals before rendering (only in 3D for now)
+    if (dim==3):
+        phi.createMesh(mesh)
+        #mesh.save('phi%04d.bobj.gz' % t)
     
     s.step()
     #gui.pause()
+	#gui.screenshot( 'screenOn_%04d.png' % t );
+
+
+
