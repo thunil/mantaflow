e2c6a149963aaa3d18c8733cd25c75b052e61462
thunil <thuereyn@inf.ethz.ch>
2013-09-05 20:29:19 -0700

    first version of simple flip test case
    --HG--
    branch : flipExtensions
    extra : rebase_source : 21165e635010d94ab21d08e0253b74dadf759adc

---
+++ scenes/flip_simple.py
@@ -0,0 +1,61 @@
+#
+# Very simple flip without level set
+# and without any particle resampling
+# 
+from manta import *
+
+# solver params
+dim = 3
+particleNumber = 2
+res = 64
+gs = vec3(res,res,res)
+if (dim==2):
+	gs.z=1
+	particleNumber = 3 # use more particles in 2d
+s = Solver(name='main', gridSize = gs, dim=dim)
+s.timestep = 0.5
+
+# prepare grids and particles
+flags    = s.create(FlagGrid)
+vel      = s.create(MACGrid)
+phiInit  = s.create(LevelsetGrid)
+pressure = s.create(RealGrid)
+flip     = s.create(FlipSystem) 
+
+# scene setup
+flags.initDomain(boundaryWidth=0)
+#fluidbox = s.create(Box, p0=gs*vec3(0,0,0), p1=gs*vec3(0.4,0.6,1))
+fluidbox = s.create(Box, p0=gs*vec3(0.4,0.4,0.4), p1=gs*vec3(0.6,0.8,0.6))
+phiInit = fluidbox.computeLevelset()
+flags.updateFromLevelset(phiInit)
+# phiInit is not needed from now on!
+
+# note, there's no resamplig here, so we need _LOTS_ of particles...
+flip.initialize( flags=flags, discretization=particleNumber, randomness=0.2 )
+    
+if (GUI):
+    gui = Gui()
+    gui.show()
+    gui.pause()
+    
+#main loop
+for t in range(2500):
+    
+    # FLIP 
+    flip.advectInGrid(flaggrid=flags, vel=vel, integrationMode=IntRK4)
+    flip.velocitiesToGrid(vel=vel, flags=flags)
+    flip.markFluidCells(flags=flags)
+    
+    addGravity(flags=flags, vel=vel, gravity=(0,-0.002,0))
+    
+    # pressure solve
+    setWallBcs(flags=flags, vel=vel)    
+    solvePressure(flags=flags, vel=vel, pressure=pressure)
+    setWallBcs(flags=flags, vel=vel)
+    
+    # FLIP load
+    flip.velocitiesFromGrid(vel=vel, flags=flags, flipRatio=0.97)
+    
+    #gui.screenshot( 'flipout_%04d.png' % t );
+    s.step()
+
