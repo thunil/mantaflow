41a0feb7f352d8a6e794168d415fa181c8e07660
thunil <nils@thuerey.de>
2014-07-29 10:57:15 +0200

    first symmetrized version of advection, added checksym/enforce init plugin

--- source/plugin/initplugins.cpp
+++ source/plugin/initplugins.cpp
@@ -103,45 +103,68 @@ PYTHON void checkSymmetry( Grid<Real>& a, Grid<Real>* err=NULL, bool symmetrize=
 		}
 	}
 }
-PYTHON void checkSymmetryVec3( Grid<Vec3>& a, Grid<Real>* err=NULL, bool symmetrize=false )
+PYTHON void checkSymmetryVec3( Grid<Vec3>& a, Grid<Real>* err=NULL, bool symmetrize=false, int disable=0 )
 {
 	if(err) err->setConst(0.);
 
 	//FOR_IJK(a) { a(i,j,k)[0] = 0.; }
 
 	// only along x for now!
-	if(1) {
-	const int s = a.getSize()[0];
-	FOR_IJK(a) { 
-		Vec3 v1=a(i    ,j,k);
-		Vec3 v2=a(s-1-i,j,k);
-		v1[0] *=  0.;
-		v2[0] *=  0.;
-		if(err) (*err)(i,j,k) += fabs( (double)( norm(v1 - v2) ) ); 
-		if(symmetrize && (i<s/2)) {
-			a(i,j,k)[1] = a(s-1-i,j,k)[1];
-			a(i,j,k)[2] = a(s-1-i,j,k)[2];
+	// each dimension is measured separately for flexibility (could be combined)
+
+	// x
+	if(! (disable&1) ) {
+		const int s = a.getSize()[0]+1; 
+		FOR_IJK(a) { 
+			if(s-1-i >= a.getSize()[0]) continue; 
+			if(s-1-i == i             ) continue; 
+			Vec3 v1 = a(i    ,j,k);
+			Vec3 v2 = a(s-1-i,j,k);
+			v1[1] = v2[1] = 0.;
+			v1[2] = v2[2] = 0.;
+			v1[0] *=  1.;
+			v2[0] *= -1.;
+			if(err) (*err)(i,j,k) += fabs( (double)( norm(v1 - v2) ) ); 
+			if(symmetrize && (i<s/2)) {
+				a(i,j,k)[0] = -a(s-1-i,j,k)[0] + 0.;
+			}
 		}
 	}
+
+	// y
+	if(! (disable&2) ) {
+		const int s = a.getSize()[0];
+		FOR_IJK(a) { 
+			Vec3 v1=a(i    ,j,k);
+			Vec3 v2=a(s-1-i,j,k);
+			v1[0] = v2[0] = 0.;
+			v1[2] = v2[2] = 0.;
+			v1[1] *=  1.;
+			v2[1] *=  1.;
+			if(err) (*err)(i,j,k) += fabs( (double)( norm(v1 - v2) ) ); 
+			if(symmetrize && (i<s/2)) {
+				a(i,j,k)[1] = a(s-1-i,j,k)[1];
+			}
+		}
 	} 
 
-	if(1){
-	const int s = a.getSize()[0]+1; 
-	FOR_IJK(a) { 
-		if(s-1-i >= a.getSize()[0]) continue; 
-		if(s-1-i == i             ) continue; 
-		Vec3 v1 = a(i    ,j,k);
-		Vec3 v2 = a(s-1-i,j,k);
-		v1[1] = v2[1] = 0.;
-		v1[2] = v2[2] = 0.;
-		v1[0] *=  1.;
-		v2[0] *= -1.;
-		if(err) (*err)(i,j,k) += fabs( (double)( norm(v1 - v2) ) ); 
-		if(symmetrize && (i<s/2)) {
-			a(i,j,k)[0] = -a(s-1-i,j,k)[0] + 0.;
+	// z
+	if(! (disable&4) ) {
+		const int s = a.getSize()[0];
+		FOR_IJK(a) { 
+			Vec3 v1=a(i    ,j,k);
+			Vec3 v2=a(s-1-i,j,k);
+			v1[0] = v2[0] = 0.;
+			v1[1] = v2[1] = 0.;
+			v1[2] *=  1.;
+			v2[2] *=  1.;
+			if(err) (*err)(i,j,k) += fabs( (double)( norm(v1 - v2) ) ); 
+			if(symmetrize && (i<s/2)) {
+				a(i,j,k)[2] = a(s-1-i,j,k)[2];
+			}
 		}
-	}
-	}
+	} 
+
 }
 
 // only along x for now!
