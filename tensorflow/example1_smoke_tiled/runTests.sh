f38771b273eb5c2d10299d66c3d3625371b2478b
Daniel Hook <daniel@hook.de>
2017-03-27 18:30:39 +0200

    fixed folder deletion (removed slash and changed command)

--- tensorflow/example1_smoke_tiled/runTests.sh
+++ tensorflow/example1_smoke_tiled/runTests.sh
@@ -5,7 +5,7 @@
 # expects training data in sim 1000, and applies to 2007 and 2008 (shortened data sets)
 
 BPATH=../data/
-SAFEDEL=rm
+SAFEDEL='rm -r'
 SCENE_TRAIN=1000
 SCENE_NP=2008
 SCENE_UNI=2007
@@ -19,8 +19,8 @@ echo Using data path ${BPATH}
 #find ${BPATH}/sim_${SCENE_NP}/    -iname "tiles*" -exec rm -fr \{\} \;
 
 # remove old output dir
-${SAFEDEL} ${BPATH}/test_0700
-${SAFEDEL} ${BPATH}/test_0800
+${SAFEDEL} ${BPATH}test_0700
+${SAFEDEL} ${BPATH}test_0800
 
 # train a model
 python tf_model_np.py out 0 basePath ${BPATH}  useVelocities 0  trainingEpochs 1000   alwaysSave 1  testPathStartNo 700 fromSim ${SCENE_TRAIN}        simSizeLow 64
@@ -31,11 +31,10 @@ python tf_model_np.py out 1 basePath ${BPATH}  useVelocities 0  testPathStartNo 
 
 # same for a model using velocities
 
-${SAFEDEL} ${BPATH}/test_0710
-${SAFEDEL} ${BPATH}/test_0810
+${SAFEDEL} ${BPATH}test_0710
+${SAFEDEL} ${BPATH}test_0810
 
 python tf_model_np.py out 0 basePath ${BPATH}  useVelocities 1  trainingEpochs 1000  alwaysSave 1  testPathStartNo 710 fromSim ${SCENE_TRAIN}         simSizeLow 64
-
 python tf_model_np.py out 1 basePath ${BPATH}  useVelocities 1  testPathStartNo 810  fromSim ${SCENE_NP} toSim -1  loadModelTest 710 loadModelNo 0    simSizeLow 128
 
 # --- uni file tests ---
@@ -46,16 +45,16 @@ python tf_model_np.py out 1 basePath ${BPATH}  useVelocities 1  testPathStartNo 
 
 # train a model without vels
 
-${SAFEDEL} ${BPATH}/test_0720
-${SAFEDEL} ${BPATH}/test_0820
+${SAFEDEL} ${BPATH}test_0720
+${SAFEDEL} ${BPATH}test_0820
 
 python tf_model_uni.py out 0 basePath ${BPATH}   useVelocities 0  trainingEpochs 1000  alwaysSave 1 testPathStartNo 720 fromSim ${SCENE_TRAIN}         simSizeLow 64
 python tf_model_uni.py out 1 basePath ${BPATH}   useVelocities 0  testPathStartNo 820  fromSim ${SCENE_UNI} toSim -1  loadModelTest 720 loadModelNo 0  simSizeLow 128
 
 # train a model with vels
 
-${SAFEDEL} ${BPATH}/test_0730
-${SAFEDEL} ${BPATH}/test_0830
+${SAFEDEL} ${BPATH}test_0730
+${SAFEDEL} ${BPATH}test_0830
 
 python tf_model_uni.py out 0 basePath ${BPATH}   useVelocities 1  trainingEpochs 1000  alwaysSave 1 testPathStartNo 730 fromSim ${SCENE_TRAIN}         simSizeLow 64
 python tf_model_uni.py out 1 basePath ${BPATH}   useVelocities 1  testPathStartNo 830  fromSim ${SCENE_UNI} toSim -1  loadModelTest 730 loadModelNo 0  simSizeLow 128
