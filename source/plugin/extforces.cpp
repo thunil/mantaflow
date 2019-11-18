dd8240da1f8acc6d39a70035755633dc5697ffe3
Sebastián Barschkis <sebbas@sebbas.org>
2019-11-18 17:18:09 +0100

    removed obsolete obvel setter in extforces

--- source/plugin/extforces.cpp
+++ source/plugin/extforces.cpp
@@ -249,10 +249,6 @@ KERNEL() void KnSetWallBcsFrac(const FlagGrid& flags, const MACGrid& vel, MACGri
 		normalize(dphi); 
 		Vec3 velMAC = vel.getAtMACX(i,j,k);
 		velTarget(i,j,k).x = velMAC.x - dot(dphi, velMAC) * dphi.x;
-		if (obvel) { // TODO (sebbas): TBC
-			Vec3 obvelMAC = (*obvel).getAtMACX(i,j,k);
-			velTarget(i,j,k).x += dot(dphi, obvelMAC) * dphi.x;
-		}
 	}
 
 	if( curObs | flags.isObstacle(i,j-1,k) )  { 
@@ -276,10 +272,6 @@ KERNEL() void KnSetWallBcsFrac(const FlagGrid& flags, const MACGrid& vel, MACGri
 		normalize(dphi); 
 		Vec3 velMAC = vel.getAtMACY(i,j,k);
 		velTarget(i,j,k).y = velMAC.y - dot(dphi, velMAC) * dphi.y;
-		if (obvel) { // TODO (sebbas): TBC
-			Vec3 obvelMAC = (*obvel).getAtMACY(i,j,k);
-			velTarget(i,j,k).y += dot(dphi, obvelMAC) * dphi.y;
-		}
 	}
 
 	if( phiObs->is3D() && (curObs | flags.isObstacle(i,j,k-1)) )  {
@@ -304,10 +296,6 @@ KERNEL() void KnSetWallBcsFrac(const FlagGrid& flags, const MACGrid& vel, MACGri
 		normalize(dphi); 
 		Vec3 velMAC = vel.getAtMACZ(i,j,k);
 		velTarget(i,j,k).z = velMAC.z - dot(dphi, velMAC) * dphi.z;
-		if (obvel) { // TODO (sebbas): TBC
-			Vec3 obvelMAC = (*obvel).getAtMACZ(i,j,k);
-			velTarget(i,j,k).z += dot(dphi, obvelMAC) * dphi.z;
-		}
 	}
 	} // not at boundary
 
