2f56c1ecafc00f430be0c18005ac28cdb52e3700
Sebastián Barschkis <sebbas@sebbas.org>
2020-06-16 15:43:27 +0200

    OpenVDB: Remove OpenVDB==1 marco in header
    Must not hide all OpenVDB IO functions.

--- source/fileio/mantaio.h
+++ source/fileio/mantaio.h
@@ -55,10 +55,8 @@ int writeGridsVol(const std::string& name, std::vector<PbClass*>* grids);
 int writeGridsTxt(const std::string& name, std::vector<PbClass*>* grids);
 
 // OpenVDB
-#if OPENVDB==1
 int writeGridsVDB(const std::string& filename, std::vector<PbClass*>* grids, float scale=1.0);
 int readGridsVDB(const std::string& filename, std::vector<PbClass*>* grids, float scale=1.0);
-#endif
 
 // Numpy
 template<class T> void writeGridNumpy(const std::string& name, Grid<T>* grid);
